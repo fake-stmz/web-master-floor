7b15e5909bb1996edfd4b55a1bdba5b06ae060b6
fake-stmz <artur.kravchenko.2006@gmail.com>
2025-11-11 17:13:14 +0700

    Added README.md

--- web_master_floor/partners/views.py
+++ web_master_floor/partners/views.py
@@ -7,3 +7,13 @@ def partner_list(request):
     partners = Partner.objects.all()
     
     return render(request, 'partner_list.html', { 'partners': partners })
+
+def edit_partner(request, partner_id):
+    
+    partner = Partner.objects.get(id=partner_id)
+    
+    return render(request, 'edit_add.html', { 'partner': partner })
+
+def add_partner(request):
+    
+    return render(request, 'edit_add.html')
--- web_master_floor/web_master_floor/urls.py
+++ web_master_floor/web_master_floor/urls.py
@@ -21,4 +21,6 @@ from partners import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.partner_list, name='partner_list'),
+    path('edit/<int:partner_id>', views.edit_partner, name='edit_partner'),
+    path('add/', views.add_partner, name='add_partner'),
 ]
